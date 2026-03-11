17bc039f182ce87f9548bd691fd32939e8c4bd93
Ratchanon Moungwichean <mynameisnon23@gmail.com>
2026-03-11 23:49:02 +0700

    add drf w/ api/project/

--- foodlocker/admin.py
+++ foodlocker/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Project, Building, Room, Locker, LineUser, LockerLog
 
-# Register your models here.
+admin.site.register(Project)
+admin.site.register(Building)
+admin.site.register(Room)
+admin.site.register(Locker)
+admin.site.register(LineUser)
+admin.site.register(LockerLog)
